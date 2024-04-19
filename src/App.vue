df3b827216b98a9ce69fa93f5433d7936d069e6c
ante <antetc@cs.wisc.edu>
2024-04-19 09:43:06 -0500

    initial for artificially generated facial image detection and classification cv project website

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <body class="holy-grail">
+    <header class="main-header">
+      HEADER CONTENT
+    </header>
+  
+    <div class="holy-grail-body">
+  
+      <section class="holy-grail-content">
+        <!-- MAIN CONTENT -->
+      </section>
+  
+      <div class="holy-grail-sidebar-left hg-sidebar">
+        <!-- LEFT SIDEBAR -->
+      </div>
+  
+      <div class="holy-grail-sidebar-right hg-sidebar">
+        <!-- RIGHT SIDEBAR -->
+      </div>
+    </div>
+  
+    <footer class="main-footer">
+      FOOTER CONTENT
+    </footer>
+  </body>
+  </template>
+  
+  <style scoped>
+
+  </style>
+  
